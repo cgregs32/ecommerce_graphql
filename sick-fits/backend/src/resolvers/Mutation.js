a5fb6b074eaacc99415dd0a17d405635ee666364
Chris Gregory <cgregs32@gmail.com>
2019-03-10 14:47:37 -0600

    queries and mutations for items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    console.log(ctx);
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    console.log(item);
+    return item;
+  }
+};
 
 module.exports = Mutations;
