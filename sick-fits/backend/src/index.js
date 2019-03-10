1ce678ac2183c94ee7b7fe1fbec5bae7482ffdff
Chris Gregory <cgregs32@gmail.com>
2019-03-10 13:10:32 -0600

    yoga prisma, server setup

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,23 @@
 // let's go!
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// TODO user express Middleware to handle cookies (JWT)
+// TODO user express Middleware to populate current User
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  details => {
+    console.log(
+      `server is now running on port http://localhost:${details.port}`
+    );
+  }
+);
